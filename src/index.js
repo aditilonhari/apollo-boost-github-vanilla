fb10eb0999e9c55a79a29ff54dde0d0c00d8b501
Aditi <alonhari@newrelic.com>
2020-06-02 17:25:36 -0700

    Adding cross-fetch api for Node.js env

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
 import 'dotenv/config';
+import ApolloClient, { gql } from 'apollo-boost';
+import 'cross-fetch/polyfill';
 
-const userCredentials = { firstname: 'Robin' };
-const userDetails = { nationality: 'German' };
+const GET_ORGANIZATION = gql`
+  {
+    organization(login: "the-road-to-learn-react") {
+      name
+      url
+    }
+  }
+`;
 
-const user = {
-  ...userCredentials,
-  ...userDetails,
-};
+const client = new ApolloClient({
+  uri: 'https://api.github.com/graphql',
+  request: (operation) => {
+    operation.setContext({
+      headers: {
+        authorization: `Bearer ${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN}`,
+      },
+    });
+  },
+});
 
-console.log(user);
+client
+  .query({
+    query: GET_ORGANIZATION,
+  })
+  .then(console.log);
+
+console.log('done');
